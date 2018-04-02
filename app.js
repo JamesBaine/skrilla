ffa07baec1d70d96f5352a2675dd5c013d94e531
JamesBaine <jamesmbaine@gmail.com>
2018-04-02 11:38:34 -0700

    test code removed

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
 var budgetController = (function() {
 
-    var x = 23;
-
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
+  
 
 })();
 
@@ -23,12 +13,6 @@ var UIController = (function() {
 
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var z = budgetCtrl.publicTest(5);
-
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
-    }
+   
 
 })(budgetController, UIController);
