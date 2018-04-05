e6aa0416693e07fcbb9b053b26226b80fa3cb249
JamesBaine <jamesmbaine@gmail.com>
2018-04-04 23:41:01 -0700

    add item from construtor function to budget controller

--- app.js
+++ app.js
@@ -24,6 +24,42 @@ var budgetController = (function() {
         }
     }
 
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            // [1 2 3 4 5], next ID = 6
+            // [1 2 4 6 8], next ID = 9
+            // ID = last ID + 1
+
+            // create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+            
+            
+            // Create new item based on 'inc' or 'exp' type
+            if (type === "exp") {
+                newItem = new Expense(ID, des, val);
+            } else if (type === "inc") {
+                newItem = new Income(ID, des, val);
+            }
+            
+            // Push it into our data structure
+            data.allItems[type].push(newItem);
+
+            // return the new element
+            return newItem;
+
+        },
+
+        testing: function() {
+            console.log(data);
+        }
+    };
+
 })();
 
 
@@ -73,10 +109,12 @@ var controller = (function(budgetCtrl, UICtrl) {
     
 
     var ctrlAddItem = function() {
+        var input, newItem;
          // 1. Get the field input data
-         var input = UICtrl.getInput();         
+        input = UICtrl.getInput();         
 
         // 2. Add the item to the budget controller 
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
 
