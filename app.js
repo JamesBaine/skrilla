7ccef4f23ddb3f761b3e1a380b1e6b9d6298ed2d
JamesBaine <jamesmbaine@gmail.com>
2018-04-17 14:43:42 -0700

    delete button

--- app.js
+++ app.js
@@ -207,6 +207,12 @@ var UIController = (function() {
         
     };
 
+    var nodeListForEach = function(list, callback) {
+        for (var i = 0; i < list.length; i++) {
+            callback(list[i], i);
+        }
+    };
+
     return {
         getInput: function() {
             return {
@@ -281,12 +287,6 @@ var UIController = (function() {
 
             var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-            var nodeListForEach = function(list, callback) {
-                for (var i = 0; i < list.length; i++) {
-                    callback(list[i], i);
-                }
-            };
-
             nodeListForEach(fields, function(current, index) {
 
                 if (percentages[index] > 0) {
@@ -314,6 +314,22 @@ var UIController = (function() {
 
         },
 
+        changeType: function() {
+
+            var fields = document.querySelectorAll(
+                DOMstrings.inputType + ", " + DOMstrings.inputDescription + "," + DOMstrings.inputValue
+            );
+
+            nodeListForEach(fields, function(cur){
+
+                cur.classList.toggle("red-focus");
+
+                document.querySelector(DOMstrings.inputBtn).classList.toggle("red");
+
+            });
+
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -338,6 +354,8 @@ var controller = (function(budgetCtrl, UICtrl) {
         });
 
         document.querySelector(DOM.container).addEventListener("click", ctrlDeleteItem);
+
+        document.querySelector(DOM.inputType).addEventListener("change", UICtrl.changeType);
 
      };
 
